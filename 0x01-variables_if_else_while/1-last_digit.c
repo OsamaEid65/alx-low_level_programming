24d120daf65e73a6c44f9013e72c20b22ae5f9d9
OsamaEid65 <zoror861@gmail.com>
2023-03-19 13:11:00 -0700

    f

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ printf("Last digit of %d is %d and is 0\n", n, ln);
 }
 else
 {
-printf("Last digit of %d is %d and less than 6 and not 0\n", n, ln);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ln);
 }
 return (0);
 }
