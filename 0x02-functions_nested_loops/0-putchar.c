31901d7ec891d2c2536908efc97c29b606169f9d
OsamaEid65 <zoror861@gmail.com>
2023-03-21 12:42:03 -0700

    f

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+putchar (48 + 47);
+putchar (48 + 64);
+putchar (48 + 69);
+putchar (48 + 68);
+putchar (48 + 51);
+putchar (48 + 56);
+putchar (48 + 49);
+putchar (48 + 66);
+putchar ('\n');
+return (0);
+}
