bdcbd385241ae758269b6f1c8b0ffd850eaa9dbf
OsamaEid65 <zoror861@gmail.com>
2023-03-21 13:03:39 -0700

    f

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+int i = 97;
+
+while (i <= 122)
+{
+
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
