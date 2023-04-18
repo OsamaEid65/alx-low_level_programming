46aec466b7bda84873db7311ee3cc43389b3b215
OsamaEid65 <zoror861@gmail.com>
2023-04-18 14:39:20 -0700

    g

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,65 +1,6 @@
-#ifndef _0_OBJECT_LIKE_MACRO_H_
-#define _0_OBJECT_LIKE_MACRO_H_
-
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
-
-
-#endif
-
-
-=== 1 - pi.h===
-
-
-#ifndef _PI_H_
-#define _PI_H_
-
-
-#define PI 3.14159265359
-
-
-#endif
-
-
-===2-main.c===
-
-
-#include <stdio.h>
-/**
-*main - prints the name of the file
-*
-*Return: always 0
-*/
-
-
-int main(void)
-{
-   printf("%s\n", __FILE__);
-   return (0);
-}
-
-
-===3-function_like_macro.h===
-
-
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
-
-
-#define ABS(x) ((x) < 0 ? (-(x)) : (x))
-
-
-#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
-
-
-=== 4-sum.h ===
-
-
-#ifndef _SUM_H_
-#define _SUM_H_
-
-
-#define SUM(x, y) ((x) + (y))
-
 
 #endif
