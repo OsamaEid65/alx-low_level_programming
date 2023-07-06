d922e54191a325f110bc7b45486d737861ff68c0
OsamaEid65 <zoror861@gmail.com>
2023-07-05 18:06:05 -0700

    a

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print string in reverse way good
+ * @s: pointer to string
+ * Return:return strint in reverse
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+_putchar('\n');
+else
+{
+_puts_recursion(s + 1);
+_putchar(*s);
+}
+}
