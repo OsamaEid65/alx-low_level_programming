890969373a3ef68eed61bbb758cfa6be51a6976e
OsamaEid65 <zoror861@gmail.com>
2023-07-07 06:06:36 -0700

    g

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
-int main ( int argc, char * argv[] )
-{
-	int i ;
+/**
+ * main - the fucation is good so good
+ * @argc:is ggod prameter
+ * @argv:is more good and good
+ * Return:return always 0
+ */
 
-	for (i=0;i<argc;i++)
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n",argv[i]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
