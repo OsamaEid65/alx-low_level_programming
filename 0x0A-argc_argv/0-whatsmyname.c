92172286fad26967caf26347026d5f2f72a65604
OsamaEid65 <zoror861@gmail.com>
2023-07-07 05:07:06 -0700

    f

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,22 @@
+/**
+ * main - is good fucvcation and so helthy
+ * @argc:ine prameter count
+ * @argv:is ggod prameter so good
+ * Return:return the prameter value
+ */
+
+
 #include <stdio.h>
 
-int main(int argc , char * argv[])
+int main(int argc, char *argv[])
 {
 	int i;
-	for (i=0;i<argc;i++)
+
+	for (i = 0; i < argc; i++)
+
 	{
-		printf("%s\n",argv[i]);
+		printf("%s\n", argv[i]);
 	}
 
-	return(0);
+	return (0);
 }
